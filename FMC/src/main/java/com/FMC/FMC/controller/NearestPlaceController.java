9b704ea052c4047300f82f0427910d3583a4cff2
Wleszcz <wiktor.leszczynski@tlen.pl>
2025-03-13 14:42:31 +0100

    get-amenity-list

--- FMC/src/main/java/com/FMC/FMC/controller/NearestPlaceController.java
+++ FMC/src/main/java/com/FMC/FMC/controller/NearestPlaceController.java
@@ -1,13 +1,16 @@
 package com.FMC.FMC.controller;
 
+import com.FMC.FMC.Amenity;
 import com.FMC.FMC.clients.OpenRouteServiceClient;
 import com.FMC.FMC.clients.OverpassClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -45,6 +48,27 @@ public class NearestPlaceController {
                             });
                 });
     }
+
+    @GetMapping("/get-amenity-list")
+    public Mono<List<Map<String, Object>>> getIndex(@RequestParam double lat, @RequestParam double lon) {
+        return Flux.fromStream(Arrays.stream(Amenity.values()))
+                .flatMap(t -> overpassClient.findNearestPlace(lat, lon, t.name().toLowerCase())
+                        .flatMap(place -> {
+                            double placeLat = (double) place.get("lat");
+                            double placeLon = (double) place.get("lon");
+
+                            return orsClient.getTravelTime(lat, lon, placeLat, placeLon)
+                                    .map(travelData -> {
+                                        Map<String, Object> response = new HashMap<>();
+                                        response.put("type", t.name().toLowerCase());
+                                        response.put("place", place);
+                                        response.put("travel", extractTravelSummary(travelData));
+                                        return response;
+                                    });
+                        }))
+                .collectList();
+    }
+
     private Map<String, Object> extractTravelSummary(Map<String, Object> travelData) {
         Map<String, Object> travelSummary = new HashMap<>();
         if (travelData != null) {
